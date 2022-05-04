86ad72ee478d7950a0355e7962f8704d195ef36f
sugiyama ryota <sugiyama@anysense.co.jp>
2022-05-04 19:58:34 +0900

    Add fetch graphQL

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,34 @@ import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import fetchGraphQL from '../lib/fetchGraphQL';
 
 const Home: NextPage = (props) => {
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        repository(owner: "sugiyamaryota" name: "next-template-2022") {
+          name
+        }
+      }
+    `).then(response => {
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      setName(data.repository.name);
+    }).catch(error => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [fetchGraphQL]);
 
   fetch('https://myapi.dev/reviews')
     .then((res) => res.json())
@@ -20,7 +46,7 @@ const Home: NextPage = (props) => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
